4be3ec1bb811c6db01a3e310981b0ba99644ab74
Diego Nunes <nunesdiegond@gmail.com>
2022-04-08 14:50:20 -0300

    primeiro comit

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = props => {
+    return (
+        <div className='menu'>
+            <ul>
+                <li><a href="#">Home</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Menu;
